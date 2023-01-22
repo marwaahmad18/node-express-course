61571d91ed7f635a7ea0362342bcf90c874c2a51
Marwa365 <92280211+marwaahmad18@users.noreply.github.com>
2023-01-22 12:00:20 -0800

    Week 3 assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,4 +7,4 @@ const _ = require ("lodash")//this _ is commonly used as a varibale name to pack
 const items = [1, [2, [3, [4]]]] //this is called the flatten deep method, located in lodash, makes it possible to return these as flat array.
 const newItems = _.flattenDeep(items);// Remember, we used _. to access it the available modules.
 console.log(newItems);
-console.log('hello people')
+console.log('hello everybody!')
