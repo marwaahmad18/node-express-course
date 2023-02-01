597fa84cf34aa2a08d64202a57efaf26f53debb2
Marwa365 <92280211+marwaahmad18@users.noreply.github.com>
2023-01-31 18:32:42 -0800

    Revert "Week 4 assignment"
    This reverts commit e62997bc62416e13d806e846c10baa35cd9e3110.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,34 +1,58 @@
-// Methods
+//
 
-//1) HTTP method, reading data: GET
 const express = require('express')
+const { escapeRegExp } = require('lodash')
 const app = express()
-let {people} = require('./data')
 
-//2) Get access to the Static file method, to be used later to complete our POST req.
-app.use(express.static('./methods-public'))//To have access to z 2x .html & 2x .css files prepared by z tutor.
+const {products} = require('./data')
 
-
-//3) Parse form data, which is going to parse the sent data & add z value to req.body
-app.use(express.urlencoded({extended: false}))
-
-app.get('/api/people', (req, res) =>{
-  res.status(200).json({success: true, data: people})
+app.get('/', (req, res)=> {
+  res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
 })
 
-app.post('/api/people', (req, res)=>{
-  res.status(201).send('Success')
+app.get('/api/products', (req, res)=> {
+  const newProducts = products.map((product)=>{
+    const {id, name, image}= product//use exactly same objects name used inside data.js, for each object that represents a product
+    return{id, name, image}//Must return it.
+  })
+  res.json(newProducts)//change from products to newProduct
 })
 
-app.post('/login', (req, res)=>{//The '/login' route is what the instructor has set up for the form
-                                  //His 2x .js examples will be manipulating the people object.
-  //console.log(req.body)
-  const {name} = req.body;
-  if (name){
-    return res.status (200).send(`Welcome ${name}`)
+app.get('/api/products/:productID', (req, res)=> {
+  // console.log(req);
+  // console.log(req.params);
+  const {productID} = req.params//This will fetch me the productID but for now, it's a string. To fix this, use:
+  const singleProduct = products.find(
+    (product)=> product.id=== Number(productID))//change #1 to this.
+
+  if (!singleProduct){//Exclamation to say: if singleProduct doesn't exist
+    return res.status(404).send('Product does not exist')
   }
+    return res.json(singleProduct)//return that product.
+})
+//Getting more complex with our param.
+app.get('/api/products/:productID/reviews/:reviewID', (req, res)=> {
+  console.log(req.params);
+  res.send('Hello world')
+})
 
-  res.status(401).send('Pleast provide your credentials')//If no value was sent through the Form
+app.get('/api/v1/query', (req, res) => {
+  const {search, limit}= req.query//instead of console.log(req.query) z query. I'm looking for 2 keys. 
+  let sortedProducts = [...products];//This is my new array. I used let bcz we'll b modifying this value in the future.
+  //We still need to filter our search:
+  if (search){
+    sortedProducts = sortedProducts.filter((product)=>{
+      return product.name.startsWith(search)
+    })
+  }
+  if (limit){
+    sortedProducts = sortedProducts.slice(0, Number(limit))//now we're talking numbers.
+  }
+  if (sortedProducts.length <1){
+    // res.status(200).send('Sorry, no product match your search query')
+    return res.status(200).json({success:true, data:[]})
+  }
+  res.status(200).json(sortedProducts)
 })
 
 app.listen(5000, ()=>{
